055257d1503fa72a3fcdf8df6ad7bedfdd28bddf
Aimen EL ASSIMI <aelassim@IFREMER.FR>
2022-10-06 14:55:43 +0200

    Début et ouverture de la fonction carte

---
+++ scripts/fonction_carte.R
@@ -0,0 +1,54 @@
+################################################################################
+# Le but de la fonction est de pouvoir visualiser des profils verticaux
+# en entrant le paramètre et les coordonnées.
+################################################################################
+graphics.off()
+rm(list=ls())
+
+gf<-function(){graphics.off()}
+
+library(ggplot2)
+library(ncdf4)
+library(fields)
+library(dplyr)
+library(tidyr)
+# POLCOM-ERSEM
+# Janvier 1985
+################################################################################
+####### Exemple avec données minimales #########################################
+################################################################################
+dirpolcom<-"data/"
+list.files(dirpolcom)
+
+ncf<-nc_open(paste0(dirpolcom,"CERES_outputs_IFREMER_Aug22.rcp85.2020.07.nc"))
+
+nm<-attributes(ncf$var)$names
+nm
+
+lsimu<-lapply(nm,ncvar_get,nc=ncf)
+names(lsimu)<-nm
+
+lapply(lsimu,dim)
+
+# dealing with missing values (above 9.9...e+36 )--> NAs
+missvalf<-function(x){
+  x[x>1E36]<-NA 
+  return(x)
+}
+lsimu<-lapply(lsimu,missvalf)
+################################################################################
+################################################################################
+
+vertical_profile <- function(simulation, parameter, longitude, 
+                             latitude, depth = 0 ,  xlab = parameter, ylab = "Depth" ){
+  measure <- simulation[[parameter]][longitude,latitude,]
+  max<- max(measure)
+  min<- min(measure)  
+  par(mfrow=c(1,1))
+  plot <- qplot(x=measure, y=simulation$depth[longitude,latitude,], xlab = xlab, ylab = ylab) + # Pour voir la thermocline avec la profondeur déscendante en prenant les vraies valeurs pas besoin d'inverser la courbe
+    scale_x_continuous(name = xlab, limits = c(min,max))+  # Pour voir la variation il faut que les bornes soient cohérentes
+    
+    geom_line() # L'equation qui fit les points pas necessaire mais bon.
+  return(plot)
+}
+
