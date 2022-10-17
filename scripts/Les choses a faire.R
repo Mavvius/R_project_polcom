c87cd7fff215a160ba6030f8632dcebac278d74f
Aimen EL ASSIMI <aelassim@IFREMER.FR>
2022-10-17 17:07:15 +0200

    To do list pour un moment

---
+++ scripts/Les choses a faire.R
@@ -0,0 +1,9 @@
+##########################
+# Les choses a faire, 
+# - "intégration verticale." faire la somme des valeurs pondérés sur la colonne d'eau
+#   Cad ponderer par la hauteur de la case. (Deux options 1 si les valeurs sont egales tout sommer
+#    option 2 si les valeurs sont inégales ponderer sur la hauteur de case.)
+# - Fonction de conversion de profondeur. parametre profondeur (en m). en cases "bottom" 40 "surface" 1
+# - Pour la somme etape 1 -> trouver la case la plus proche
+#                 etape 2 -> trouver la distance la plus proche et ponderer le morceau pris sur la vraie taille de la case. 
+##################
