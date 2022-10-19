3cafc4fbccd3fe1c24905796135a5c3e87f4518c
Aimen EL ASSIMI <aelassim@IFREMER.FR>
2022-10-19 16:24:35 +0200

    Apply fonctionne pour faire l'intégration verticale partie 1 -> faire la somme de la colonne

--- scripts/Les choses a faire.R
+++ scripts/Les choses a faire.R
@@ -7,3 +7,54 @@
 # - Pour la somme etape 1 -> trouver la case la plus proche
 #                 etape 2 -> trouver la distance la plus proche et ponderer le morceau pris sur la vraie taille de la case. 
 ##################
+
+
+lsimu$P1c[55,55,]
+sum(which(lsimu$depth[55,55,] < -100))
+?sum
+
+lsimu$depth[55,55,][31]
+
+# Permet de trouver quels valeurs satisfont une condition (indice)
+vect_dep<- which(lsimu$depth[55,55,] < -100)
+lsimu$depth[55,55,][31] # indice de la valeur voulue
+sum(lsimu$depth[55,55,][vect_dep])
+
+
+temp <- which(lsimu$ETW[,,] > 17)
+sum(lsimu$ETW[,,][temp])
+str(lsimu$ETW)
+dim(lsimu$ETW[,,])[1]
+
+sum_vect <- vector()
+
+for( i in 1:dim(lsimu$ETW)[1]){
+  for (j in 1:dim(lsimu$ETW)[2]){
+    som <- sum(lsimu$ETW[i,j,], na.rm = F)
+    sum_vect <- append(sum_vect, som)
+  }
+}
+?append
+plot(sum_vect)
+
+lsimu$ETW[55,55,4]
+
+ma <- matrix(c(1:24), ncol = 3, nrow = 8)
+
+ma
+apply(ma, 1, table)  #--> a list of length 2
+apply(ma, 1, stats::quantile) # 5 
+
+température <- lsimu$ETW[50:55, 50:55, ]
+température
+mapply(sum(x), température[,,x], dim(température)[3])
+
+température[1,1,]
+sum(température[1,1,])
+apply(température, c(1:2),sum) # Ca fonctionne 
+?apply
+
+
+sum(température[6,6,])
+
+integration_verticale <- function(simulation, parameter, above, below, area )
